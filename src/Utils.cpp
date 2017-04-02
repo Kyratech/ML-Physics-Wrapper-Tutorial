89e2b9a6ed3051648c9890e16a348a416c934e32
Mattadon <ml2g14@soton.ac.uk>
2017-04-02 20:55:53 +0100

    Changed the initial camera position to be more interesting

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -27,11 +27,9 @@ GLFWwindow *window;
 //Set up the camera//
 //-----------------//
 
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+glm::vec3 cameraPos = glm::vec3(0.0f, 15.0f, 15.0f);
 
-ThreeD_Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+ThreeD_Camera camera(cameraPos);
 
 GLfloat lastX = WIDTH / 2.0;
 GLfloat lastY = HEIGHT / 2.0;
