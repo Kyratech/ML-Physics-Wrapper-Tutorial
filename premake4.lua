4f0a437f89dc10250957baa996d24053e17532ac
Mattadon <ml2g14@soton.ac.uk>
2017-04-02 20:12:33 +0100

    Physics wrapper library name changed, so updated premake script

--- premake4.lua
+++ premake4.lua
@@ -7,6 +7,6 @@ solution ('BulletWrapperTesterCB') --Top Level name of the project
       project("WrapperTesterCB") -- The specific name
         kind 'ConsoleApp' -- the target application
         targetdir('./bin/') -- where to put the executable
-        links{'glew32', 'glfw3', 'opengl32', 'libBulletWrapper', 'libBulletDynamics', 'libBulletCollision', 'libLinearMath', 'gdi32'} -- the libraries to link
+        links{'glew32', 'glfw3', 'opengl32', 'libPhysicsWrapper', 'libBulletDynamics', 'libBulletCollision', 'libLinearMath', 'gdi32'} -- the libraries to link
         files {"src/*.cpp"} -- collects all the cpp files in the directory, there must only be one main function.
         buildoptions{'-Wno-write-strings'} -- build option to suppress a very common warning  about strings
